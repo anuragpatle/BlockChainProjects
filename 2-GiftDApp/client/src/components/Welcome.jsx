7bd6adc912edf0971357231c016b5dd6654927d9
anuragpatle <anurag.ap6@gmail.com>
2022-04-27 20:05:07 +0530

    2-GiftDApp- Connect account to wallet

--- 2-GiftDApp/client/src/components/Welcome.jsx
+++ 2-GiftDApp/client/src/components/Welcome.jsx
@@ -7,9 +7,8 @@ import { TransactionContext } from "../context/TransactionContext";
 // import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
-
-
-const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const companyCommonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
@@ -23,10 +22,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-//   const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
-  const { value } = useContext(TransactionContext);
-
-  console.log("###value: ", value); // will print test in the console
+  //   const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
+  const { currentAccount, connectWallet } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
@@ -46,20 +43,21 @@ const Welcome = () => {
             Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+            Explore the crypto world. Buy and sell cryptocurrencies easily on
+            Krypto.
           </p>
-          {/* {!currentAccount && (
+          {!currentAccount && (
             <button
               type="button"
               onClick={connectWallet}
-              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              className="flex w-full flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
               <AiFillPlayCircle className="text-white mr-2" />
               <p className="text-white text-base font-semibold">
                 Connect Wallet
               </p>
             </button>
-          )} */}
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -99,6 +97,10 @@ const Welcome = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input placeholder="Address To" name="addressTo" type="text" />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
+            <Input placeholder="Enter Message" name="message" type="text" />
             {/* <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
@@ -106,17 +108,17 @@ const Welcome = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false
-              ? <Loader />
-              : (
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                  Send now
-                </button>
-              )}
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send now
+              </button>
+            )}
           </div>
         </div>
       </div>
--- 2-GiftDApp/client/src/context/TransactionContext.jsx
+++ 2-GiftDApp/client/src/context/TransactionContext.jsx
@@ -23,8 +23,46 @@ const getEthereumContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
+  // const [connectedAccounts, setconnectedAccounts] = useState(initialState);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install metamask");
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    console.log("##accounts: ", accounts);
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install MetaMask.");
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+	  console.log("eth_requestAccounts: ", accounts)
+    //   setCurrentAccount(accounts[0]);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
+
+  // By using this Hook, you tell React that your component needs to do something after render.
+  // React will remember the function you passed (we’ll refer to it as our “effect”), and call
+  // it later after performing the DOM updates. In this effect, we set the document title,
+  // but we could also perform data fetching or call some other imperative API.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <TransactionContext.Provider value={{ value: "test" }}>
+    //   Since the key and value i.e. connectWallet: connectWallet are same, we can pass just connectWallet
+    <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
       {children}
     </TransactionContext.Provider>
   );
