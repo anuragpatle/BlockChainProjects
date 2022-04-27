4b9bf864a534106339b1b7ba621eb70f0ea31b71
anuragpatle <anurag.ap6@gmail.com>
2022-04-27 16:32:32 +0530

    2-GiftDApp-Deployment, in Alchemy
    https://dashboard.alchemyapi.io/signup/app
    
    Create your first app
    An app is your gateway to Ethereum. No more running nodes - your app is all you need to get started.
    
    TEAM NAME - 2-GiftDApp
    APP NAME - 2-GiftDApp
    NETWORK - Ropsten
    
    -----------------
    To deploy:
    
    ..smart_contract$ npx hardhat run scripts/deploy.js --network ropsten
    
    Greeter deployed to: 0x4B3046CDa0902d362D97D31159c34C357Cf3851d

--- 2-GiftDApp/smart_contract/hardhat.config.js
+++ 2-GiftDApp/smart_contract/hardhat.config.js
@@ -1,3 +1,5 @@
+// https://dashboard.alchemyapi.io/apps/iigz3d0uvprfzypj
+
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -16,6 +18,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+// url: is url of the application you created using Alchemy website
+// accounts: is not the metamask address, rather it is the matamask private key.
+// For private key: "metamask addon" --> "three dots" --> "acount details" --> "export private key"
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/XniA4dRHHlKRaYEKCR9uPUQYuHSg7x3o',
+      accounts: ['0ae9094d61105fc584ded4e6d75fafbdd33a0e232a37ff39bc6be30596322777']
+    }
+  }
 };
