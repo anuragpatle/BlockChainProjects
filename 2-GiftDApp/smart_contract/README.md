a5af71e9aad7a1366f04b5b716f776bd6e423289
anuragpatle <anurag.ap6@gmail.com>
2022-05-01 09:41:54 +0530

    2-GiftDApp-Add code for ganache

--- 2-GiftDApp/smart_contract/README.md
+++ 2-GiftDApp/smart_contract/README.md
@@ -4,6 +4,8 @@ This project demonstrates a basic Hardhat use case. It comes with a sample contr
 
 Try running some of the following tasks:
 
+
+
 ```shell
 npx hardhat accounts
 npx hardhat compile
@@ -12,4 +14,31 @@ npx hardhat test
 npx hardhat node
 node scripts/deploy.js
 npx hardhat help
+```
+
+
+```
+npm init vite@latest
+
+npm install -D tailwindcss
+
+npx tailwindcss init -p
+
+cd .\2-GiftDApp\smart_contract\
+
+
+
+cd .\2-GiftDApp\client\
+npm install react-icons --save
+npm install tailwindcss --save
+npm install tailwindcss/forums --save
+npm install tailwindcss/forums --save
+npm install --sav-dev hardhat @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers
+npx hardhat run scripts/deploy.js --network ropsten
+
+
+npm install --save ethers
+
+..\client$ npm run dev
+
 ```
--- 2-GiftDApp/smart_contract/hardhat.config.js
+++ 2-GiftDApp/smart_contract/hardhat.config.js
@@ -18,15 +18,30 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+
+
+// For Alchemy
 // url: is url of the application you created using Alchemy website
 // accounts: is not the metamask address, rather it is the matamask private key.
 // For private key: "metamask addon" --> "three dots" --> "acount details" --> "export private key"
+
+// For Ganache
+
+
 module.exports = {
   solidity: "0.8.0",
+  defaultNetwork: "ganache",
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/XniA4dRHHlKRaYEKCR9uPUQYuHSg7x3o',
       accounts: ['0ae9094d61105fc584ded4e6d75fafbdd33a0e232a37ff39bc6be30596322777']
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: ['200b880e7f36870396065242e64b2b9fe8bae657ab7e1a988fa1e6cef0d9dece', 'd1bb92a98fe7ec2d1c18e24078ab25987b20b663e5f518f449767da9cee7862c']
     }
   }
 };
